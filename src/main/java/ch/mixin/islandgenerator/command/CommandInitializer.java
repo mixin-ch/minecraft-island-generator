592961523aa6b2f80c6f61064218e4b4558a3fbc
Dokzentir <mischa.iwasaki@gmail.com>
2021-05-22 19:42:01 +0200

    blank

--- src/main/java/ch/mixin/islandgenerator/command/CommandInitializer.java
+++ src/main/java/ch/mixin/islandgenerator/command/CommandInitializer.java
@@ -3,7 +3,6 @@ package ch.mixin.islandgenerator.command;
 import ch.mixin.islandgenerator.main.IslandGeneratorPlugin;
 
 public class CommandInitializer {
-
     public static void setupCommands(IslandGeneratorPlugin plugin) {
         RootCommand rootCommand = new RootCommand(plugin, "mx-ig");
         plugin.getCommand("mx-ig").setExecutor(rootCommand);
