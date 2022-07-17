905e72f368f70c8802951267e51c68f3511a6ac6
Nixtrin <mischa.iwasaki@gmail.com>
2022-07-17 20:25:13 +0200

    Chest look at Gamemode

--- src/main/java/ch/mixin/islandgenerator/eventListener/EventListener.java
+++ src/main/java/ch/mixin/islandgenerator/eventListener/EventListener.java
@@ -6,6 +6,7 @@ import ch.mixin.islandgenerator.metaData.IslandData;
 import ch.mixin.islandgenerator.metaData.MetaData;
 import ch.mixin.islandgenerator.metaData.WorldData;
 import ch.mixin.islandgenerator.model.Coordinate3D;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -37,13 +38,16 @@ public class EventListener implements Listener {
     public void manipulate(PlayerInteractEvent e) {
         Block block = e.getClickedBlock();
 
-        if (block == null) {
+        if (block == null)
             return;
-        }
 
-        if (!block.getType().equals(Material.CHEST)) {
+        if (!block.getType().equals(Material.CHEST))
             return;
-        }
+
+        if (e.getPlayer().getGameMode() != GameMode.ADVENTURE
+                && e.getPlayer().getGameMode() != GameMode.CREATIVE
+                && e.getPlayer().getGameMode() != GameMode.SURVIVAL)
+            return;
 
         World world = block.getWorld();
         String worldName = world.getName();
